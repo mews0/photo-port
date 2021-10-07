33b76aecfcbbe6909b990baa3a499fae8089f4d5
Jeff Kelling <jeff.kelling.1@gmail.com>
2021-10-07 13:09:20 -0500

    Test nav and contact components

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,16 +1,24 @@
 import React, { useState } from 'react';
+
 import { validateEmail } from '../../utils/helpers';
 
 function ContactForm() {
   const [formState, setFormState] = useState({ name: '', email: '', message: '' });
-  const { name, email, message } = formState;
-  const [errorMessage, setErrorMessage] = useState('');
 
-  function handleChange(e) {
+  const [errorMessage, setErrorMessage] = useState('');
+  const { name, email, message } = formState;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errorMessage) {
+      setFormState({ [e.target.name]: e.target.value });
+      console.log('Form', formState);
+    }
+  };
+
+  const handleChange = (e) => {
     if (e.target.name === 'email') {
       const isValid = validateEmail(e.target.value);
-      console.log(isValid);
-      // isValid conditional statement
       if (!isValid) {
         setErrorMessage('Your email is invalid.');
       } else {
@@ -23,39 +31,30 @@ function ContactForm() {
         setErrorMessage('');
       }
     }
-    if (!errorMessage) {
-      setFormState({ ...formState, [e.target.name]: e.target.value });
-    }
-    console.log('errorMessage', errorMessage);
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log(formState);
-  }
+  };
 
   return (
     <section>
-      <h1>Contact me</h1>
+      <h1 data-testid="h1tag">Contact me</h1>
       <form id="contact-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name">Name:</label>
-          <input type="text" defaultValue={name} onBlur={handleChange} name="name" />
+          <input type="text" name="name" defaultValue={name} onBlur={handleChange} />
         </div>
         <div>
           <label htmlFor="email">Email address:</label>
-          <input type="email" defaultValue={email} name="email" onBlur={handleChange} />
+          <input type="email" name="email" defaultValue={email} onBlur={handleChange} />
         </div>
         <div>
           <label htmlFor="message">Message:</label>
-          <textarea name="message" defaultValue={message} onBlur={handleChange} rows="5" />
+          <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange} />
         </div>
         {errorMessage && (
           <div>
             <p className="error-text">{errorMessage}</p>
           </div>
         )}
-        <button type="submit">Submit</button>
+        <button data-testid="button" type="submit">Submit</button>
       </form>
     </section>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,20 +1,14 @@
-import React from "react";
-import { capitalizeFirstLetter } from "../../utils/helpers";
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
 
 function Nav(props) {
   const {
     categories = [],
     setCurrentCategory,
-    currentCategory,
     contactSelected,
-    setContactSelected
+    currentCategory,
+    setContactSelected,
   } = props;
-
-
-  const handleClick = (item) => {
-    console.log(item);
-    return item;
-  };
 
   return (
     <header className="flex-row px-1">
@@ -35,14 +29,16 @@ function Nav(props) {
           </li>
           {categories.map((category) => (
             <li
-              className={`mx-1 ${currentCategory.name === category.name && !contactSelected && `navActive`
+              className={`mx-1 ${
+                currentCategory.name === category.name && !contactSelected && 'navActive'
                 }`}
               key={category.name}
             >
-              <span onClick={() => {
-                setCurrentCategory(category);
-                setContactSelected(false);
-              }}
+              <span
+                onClick={() => {
+                  setCurrentCategory(category);
+                  setContactSelected(false);
+                }}
               >
                 {capitalizeFirstLetter(category.name)}
               </span>
